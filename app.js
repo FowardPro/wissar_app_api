5ae7abbc946022b36ed9b65dc97ba6686499e0bd
FowardPro <fowardnkuna6@gmail.com>
2024-08-14 15:50:51 +0200

    Some changes

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ require('dotenv').config(); // Load environment variables
 const express = require('express'); // Initialize express
 const app = express(); // Create an Express application
 require('./config/config');
+const bodyParser = require('body-parser');
 const port = process.env.PORT
+const cors = require('cors');
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}))
 app.use('/',(req,res)=>{
     res.send('end point')
 })
